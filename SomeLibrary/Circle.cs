537d8e8da233f650246e13732deef604cd82c84e
Agsayt <111ag@rambler.ru>
2022-01-15 07:17:55 +0300

    Initial release

---
+++ SomeLibrary/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeLibrary
+{
+    class Circle
+    {
+
+        public bool IsInside(double x0, double y0, double x, double y, double radius)
+        {
+
+            double d = Math.Sqrt(Math.Pow(x0 - x, 2) + Math.Pow(y0 - y, 2));
+            if (d <= radius)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ SomeLibrary/QEquation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeLibrary
+{
+    class QEquation
+    {
+        // Класс для решения квадратного уравнения
+
+        public double[] FindRoots(double a, double b, double c, out string message)
+        {
+            double d = Math.Pow(b, 2) - 4 * a * c;
+
+            if (d < 0)
+            {
+                message = "Equation doesn't has any roots";
+                return null;
+            } 
+            else if (d == 0)
+            {
+                double x = (-b + Math.Sqrt(d)) / (2 * a);
+                message = "Equation has one root x = " + x;
+                return new double[1] { x };
+            }
+            else
+            {
+                double x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(d)) / (2 * a);
+                message = "Equation has two roots: x1 = " + x1 + "; x2 = " + x2;
+                return new double[2] { x1, x2 };
+            }
+
+        }
+    }
+}
